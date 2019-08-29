b91b38c3e56f304fac53fc666aec4ed6df583921
yuyut <89313@farnborough.ac.uk>
2019-08-29 18:38:13 +0100

    Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static Connection db = null;
+
+    public static void main(String[] args) {
+        openDatabase(".db");
+        closeDataabse();
+    }
+
+    private static void OpenDatabase(String dbFile)
+
+
 }
