19ba4f6413408af96f2b7d8d2ce74204c2975464
yuyuts <89313@farnborough.ac.uk>
2019-09-19 13:45:20 +0100

    Test 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,24 @@
 import org.sqlite.SQLiteConfig;
 
+import java.sql.DriverManager;
+
 public class Main {
     public static Connection db = null;
-    private static void openDatabse(String dbFile)
-    {
-        try{
+    private static void openDatabse(String dbFile) {
+        try {
             Class.forName("org.sqlite.JDBC");
             SQLiteConfig = new SQLiteConfig();
             config.enforceForeignKeys(true);
+            db = DriverManager.getConnection("jdbc:sqlite:resources/" + dbFile, config.toProperties());
+            System.out.println("Database Connection Sucessfully Established");
 
 
-
+        } catch (Exception exception) {
+            System.out.println("Database Connection Error" + exception.getMessage());
         }
+
+    }
+    private
 
     }
 }
