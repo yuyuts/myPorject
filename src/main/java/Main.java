9ea536f67dfe62c90044a3e7ee5f108fa2805c95
yuyuts <89313@farnborough.ac.uk>
2019-09-19 13:40:32 +0100

    Test 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,17 @@
+import org.sqlite.SQLiteConfig;
+
 public class Main {
-    public static Connecti
-    private static void openDatabse()
+    public static Connection db = null;
+    private static void openDatabse(String dbFile)
+    {
+        try{
+            Class.forName("org.sqlite.JDBC");
+            SQLiteConfig = new SQLiteConfig();
+            config.enforceForeignKeys(true);
+
+
+
+        }
+
+    }
 }
